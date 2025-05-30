4439adc82637d180c23b20edc5b2544da9f253a0
kristoffer.egefelt <kristoffer.egefelt@jppol.dk>
2025-05-30 13:45:13 +0200

    Add initial Terraform configuration for AWS Transit Gateway

---
+++ main.tf
@@ -0,0 +1,34 @@
+locals {
+
+  dns_support = var.dns_support ? "enable" : "disable"
+}
+
+##########################
+#      Data Sources      #
+##########################
+
+data "aws_vpc" "tgw-vpc" {
+  filter {
+    name   = "tag:${var.vpc-tag-filter-key}"
+    values = [var.vpc-tag-filter-value]
+  }
+}
+
+data "aws_subnet_ids" "tgw-subnets" {
+  vpc_id = data.aws_vpc.tgw-vpc.id
+  filter {
+    name   = "tag:${var.subnet-tag-filter-key}"
+    values = [var.subnet-tag-filter-value]
+  }
+}
+
+##########################
+# VPC peering connection #
+##########################
+resource "aws_ec2_transit_gateway_vpc_attachment" "default" {
+  subnet_ids         = data.aws_subnet_ids.tgw-subnets.ids
+  transit_gateway_id = var.tgw-id
+  vpc_id             = data.aws_vpc.tgw-vpc.id
+  dns_support        = local.dns_support
+
+}
