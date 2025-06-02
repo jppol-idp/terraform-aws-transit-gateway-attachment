c6644d6a0f271cdaabad3b012e42cb78cabaaa51
kristoffer.egefelt <kristoffer.egefelt@jppol.dk>
2025-06-02 15:25:06 +0200

    Update subnet_ids for transit gateway attachment resource

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ data "aws_subnets" "tgw_attachment_subnets" {
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "default" {
-  subnet_ids         = data.aws_subnets.tgw_subnets.ids
+  subnet_ids         = data.aws_subnets.tgw_attachment_subnets.ids
   transit_gateway_id = var.tgw-id
   vpc_id             = data.aws_vpc.tgw-vpc.id
   dns_support        = local.dns_support
